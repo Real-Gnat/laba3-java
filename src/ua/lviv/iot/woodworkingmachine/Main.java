985bda4a65d383c310acc6d7913ca61a44367a5d
Real-Gnat <44602305+Real-Gnat@users.noreply.github.com>
2019-04-25 08:51:25 +0300

    Add files via upload

--- src/ua/lviv/iot/woodworkingmachine/Main.java
+++ src/ua/lviv/iot/woodworkingmachine/Main.java
@@ -12,9 +12,29 @@ public class Main {
         List<WoodsworkingMachine> WoodworkingMachine = new ArrayList<>();
         WoodworkingMachine.add(new WoodsworkingMachine());
 
-        MachinesManagerlmpl manager = new MachinesManagerlmpl(WoodworkingMachine);
-
         manager.sortByPower(WoodworkingMachine);
+
+        CirucularMachine cirkul = new CirucularMachine ("cirkul" , "drawing", 30, , 20, 40);
+        CirucularMachine liniyka = new CirucularMachine ("liniyka" , "painting",  70,  ClassificationType.PRESS_FORGING, 15, 35);
+        WoodsworkingMachine cleaner = new WoodsworkingMachine ("cleaner", "drawing", 30 ClassificationType.GORIZONTAL_DRILLING);
+        WoodsworkingMachine washer = new WoodsworkingMachine ("washer","painter",  34)
+        RamusMachine processing = new RamusMachine( "procesing",  "fills in",  43);
+        RamusMachine tuborg = new RamusMachine("tuborg", "empty", 39);
+
+        manager.toAdd(cirkul);
+        manager.toAdd(liniyka);
+        manager.toAdd(cleaner);
+        manager.toAdd(washer);
+        manager.toAdd(processing);
+        manager.toAdd(tuborg);
+
+        System.out.println(manager.sortByPower(true).toString());
+        System.out.println(manager.sortByPower(false));
+        System.out.println(manager.sortByVolume(true) );
+        System.out.println(manager.sortByVolume(false));
+        System.out.println(manager.findBy(ClassificationType.PRESS_FORGING));
+        System.out.println(manager.findBy(ClassificationType.GORIZONTAL_DRILLING);
+
 
 
     }
